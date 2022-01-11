eadb17dd94d447e7c28091889f5c3ad31961d056
imHson <phamhoangson99@gmail.com>
2022-01-11 16:25:21 +0700

    cmc

--- build_installer.js
+++ build_installer.js
@@ -9,7 +9,7 @@ const msiCreator = new MSICreator({
     exe: 'NFTauto Desktop',
     name: 'SPS NFTAuto Desktop',
     manufacturer: 'Caper Team',
-    version: '2.2.10',
+    version: '2.3.2',
     outputDirectory: OUT_DIR,
     ui: {
         chooseDirectory: true,
